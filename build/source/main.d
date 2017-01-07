b29f78e1c53ac66c2a38a18ad6e8aa7bf0637552
ddews <danielricharddews@gmail.com>
2017-01-06 22:18:37 -0700

    Actually fixed the splash screen. This time also for .cia, and not just homebrew after running it twice...

--- build/source/main.d
+++ build/source/main.d
@@ -80,8 +80,7 @@ build/source/main.o: source/main.c e:/devkitPro/libctru/include/3ds.h \
  e:/devkitPro/libctru/include/c3d/lightlut.h \
  e:/devkitPro/libctru/include/c3d/renderbuffer.h \
  e:/devkitPro/libctru/include/c3d/renderqueue.h source/lodepng.h \
- build/source/vshader_shbin.h build/source/splash_png.h \
- build/source/bike_png.h
+ build/source/vshader_shbin.h build/source/bike_png.h
 
 e:/devkitPro/libctru/include/3ds.h:
 
@@ -250,7 +249,5 @@ e:/devkitPro/libctru/include/c3d/renderqueue.h:
 source/lodepng.h:
 
 build/source/vshader_shbin.h:
-
-build/source/splash_png.h:
 
 build/source/bike_png.h:
